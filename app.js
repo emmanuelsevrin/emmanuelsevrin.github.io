4ae3596922777040483b0d5d6abda52bdfae1360
Emmanuel Sevrin <emmanuel.sevrin@gmail.com>
2018-07-25 13:06:26 +0300

    Added simple app.js

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var Model = {
 }
 
 var file = new File("books/OnWritingWell");
-
-
+file.open("r");
+var str = "";
+while (!file.eof) {
+	// read each line of text
+	str += file.readln() + "\n";
+}
+file.close();
+alert(str);
 
 
 var View
